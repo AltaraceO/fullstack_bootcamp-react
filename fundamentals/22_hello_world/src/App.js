62b9605f97c9ef446dc19af6611bb993f61d670f
AltaraceO <orialtarace@gmail.com>
2021-12-12 22:38:33 +0200

    end of day

--- fundamentals/22_hello_world/src/App.js
+++ fundamentals/22_hello_world/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const data = ["hello", "world"];
+  const number1 = 5;
+  const number2 = 6;
+  const string = "I love React!";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.join(" ")}
+      <div>
+        {`${number2}` + " + " + `${number1}` + " " + "= " + (number1 + number2)}
+      </div>
+      <div>{string.length}</div>
     </div>
   );
 }
