6ec2becf97fabe816d72f3b68bc08822121c777b
AltaraceO <orialtarace@gmail.com>
2022-05-11 19:02:51 +0300

    review styling and css componenets

---
+++ review/complete_guide/src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+export const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>carInsurance</h2>
+        <div className="expense-item__price">$256.54</div>
+      </div>
+      ExpenseItem
+    </div>
+  );
+};
