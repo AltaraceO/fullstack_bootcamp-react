96349bbadf858ff71dfedd426b859d7c85ec3c02
AltaraceO <orialtarace@gmail.com>
2022-05-12 21:51:36 +0300

    controlled componenet from parent to child

--- review/complete_guide/src/components/Expenses/Expenses.jsx
+++ review/complete_guide/src/components/Expenses/Expenses.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import { Card } from "../UI/Card";
+import { ExpenseFilter } from "./ExpenseFilter";
 import { ExpenseItem } from "./ExpenseItem";
 import "./Expenses.css";
 
 export const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const getFilteredYear = (year) => {
+    setFilteredYear(year);
+  };
   return (
-    <Card className="expenses">
-      {expenses.map((el) => {
-        return (
-          <ExpenseItem
-            title={el.title}
-            date={el.date}
-            amount={el.amount}
-            key={el.id}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      {console.log("filtered Year", filteredYear)}
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onFilterYear={getFilteredYear} />
+        {expenses.map((el) => {
+          return (
+            <ExpenseItem
+              title={el.title}
+              date={el.date}
+              amount={el.amount}
+              key={el.id}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
