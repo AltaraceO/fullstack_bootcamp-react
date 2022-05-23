5b75d2d008c78e49a3e2350a5cbade05bac7fbc8
AltaraceO <orialtarace@gmail.com>
2022-05-23 17:08:40 +0300

    images

---
+++ review/food-order/src/components/Layout/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+};
