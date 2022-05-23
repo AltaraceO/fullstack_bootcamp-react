f9a2692ff9c5a2d23f82dabc2e9d49b136d9f9f5
AltaraceO <orialtarace@gmail.com>
2022-05-23 17:51:06 +0300

    map individual meals

---
+++ review/food-order/src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,14 @@
+import classes from "./MealItem.module.css";
+export const MealItem = ({ name, description, price }) => {
+  const displayPrice = `$${price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{displayPrice}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
