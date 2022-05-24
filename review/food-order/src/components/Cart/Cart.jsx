f862c9b60de74d87e520a47985aaba3979a1d3f0
AltaraceO <orialtarace@gmail.com>
2022-05-24 17:39:43 +0300

    adding items to the cart and display the same item once with the amount next to it

--- review/food-order/src/components/Cart/Cart.jsx
+++ review/food-order/src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import CartContext from "./../../store/cart-context";
 import classes from "./Cart.module.css";
 import { Modal } from "../UI/Modal";
+import CartItem from "./CartItem";
 
 export const Cart = ({ onClose }) => {
   const cartCtx = useContext(CartContext);
@@ -9,10 +10,20 @@ export const Cart = ({ onClose }) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
