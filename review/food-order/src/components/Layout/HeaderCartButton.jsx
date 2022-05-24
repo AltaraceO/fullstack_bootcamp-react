1836c5e19acee8a30676ed9f8a126b8715ac8589
AltaraceO <orialtarace@gmail.com>
2022-05-24 18:27:40 +0300

    useEffect with cleanup function

--- review/food-order/src/components/Layout/HeaderCartButton.jsx
+++ review/food-order/src/components/Layout/HeaderCartButton.jsx
@@ -1,20 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
 import CartContext from "./../../store/cart-context";
 
 export const HeaderCartButton = ({ onClick }) => {
-  const cartCtx = useContext(CartContext);
+  const [buttonHighlight, setButtonHighlight] = useState(false);
 
+  const cartCtx = useContext(CartContext);
   const { items } = cartCtx;
 
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${buttonHighlight ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonHighlight(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={onClick} className={classes.button}>
+    <button onClick={onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
