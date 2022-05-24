6fae43912dfa29b169970f2319aaad75ce5a8588
AltaraceO <orialtarace@gmail.com>
2022-05-24 15:53:58 +0300

    use Reducer to manage state in store

--- review/food-order/src/components/Layout/HeaderCartButton.jsx
+++ review/food-order/src/components/Layout/HeaderCartButton.jsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
+
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
+import CartContext from "./../../store/cart-context";
+
 export const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((acc, curr) => {
+    return curr + curr.amount;
+  }, 0);
+
   return (
     <button onClick={onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
