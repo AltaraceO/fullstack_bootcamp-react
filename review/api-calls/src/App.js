849184c53b7962145cfe64a5cc92d5b4ee8c6cd7
AltaraceO <orialtarace@gmail.com>
2022-05-26 17:28:00 +0300

    async await and loading conditions

--- review/api-calls/src/App.js
+++ review/api-calls/src/App.js
@@ -5,22 +5,24 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    fetch("http://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((e) => {
-          return {
-            id: e.episode_id,
-            title: e.title,
-            openingText: e.opening_crawl,
-            releaseDate: e.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const [loading, setLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setLoading(true);
+    const res = await fetch("http://swapi.dev/api/films/");
+
+    const data = await res.json();
+
+    const transformedMovies = data.results.map((e) => {
+      return {
+        id: e.episode_id,
+        title: e.title,
+        openingText: e.opening_crawl,
+        releaseDate: e.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setLoading(false);
   };
 
   return (
@@ -29,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loading ? <MoviesList movies={movies} /> : <p>Loading...</p>}
+        {!loading && movies.length === 0 && <p>No Data Yet</p>}
       </section>
     </React.Fragment>
   );
