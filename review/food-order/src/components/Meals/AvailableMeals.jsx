1090d1986d2105ab5f3aba642c8e0b28c931d879
AltaraceO <orialtarace@gmail.com>
2022-06-06 15:28:26 +0300

    try and catch error handeling

--- review/food-order/src/components/Meals/AvailableMeals.jsx
+++ review/food-order/src/components/Meals/AvailableMeals.jsx
@@ -6,11 +6,18 @@ import { MealItem } from "./MealItem/MealItem";
 
 export const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchMeals = async () => {
+      setLoading(true);
       const res = await fetch(
         "https://react-review-68b13-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!res.ok) {
+        throw new Error("something went wrong");
+      }
       const resData = await res.json();
       const extractedData = [];
 
@@ -24,10 +31,32 @@ export const AvailableMeals = () => {
       }
 
       setMeals(extractedData);
+      setLoading(false);
     };
 
-    fetchMeals();
+    try {
+      fetchMeals();
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
   }, []);
+
+  if (loading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <h3>Loading...</h3>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={classes.MealsError}>
+        <h3>{error}</h3>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
