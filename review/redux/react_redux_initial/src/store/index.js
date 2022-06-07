070eccb67aa617a75c12a802b52bfa03a34baf48
AltaraceO <orialtarace@gmail.com>
2022-06-07 12:47:35 +0300

    redux and react

---
+++ review/redux/react_redux_initial/src/store/index.js
@@ -0,0 +1,26 @@
+import {createStore} from 'redux'
+
+//*action is an object
+const counterReducers= (state={counter:0}, action){
+    if(action.type = 'increment'){
+        return{
+            counter: state.counter+1
+        }
+    }
+    if(action.type = 'decrement'){
+        return{
+            counter: state.counter-1
+        }
+    }
+
+    return state
+}
+
+
+
+
+const store = createStore(counterReducers)
+
+export default store
+// store.dispatch({type:'increment'})
+// store.dispatch({type:'decrement'})
