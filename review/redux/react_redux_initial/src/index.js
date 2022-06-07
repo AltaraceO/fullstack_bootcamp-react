d3dfea58f84e510b48b39a303fc562503049ccfc
AltaraceO <orialtarace@gmail.com>
2022-06-07 13:33:23 +0300

    set up the react-redux provider in the index root folder where all will have access to the store

--- review/redux/react_redux_initial/src/index.js
+++ review/redux/react_redux_initial/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//*this provides the store to where ever it is needed
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+//*The file where the store is exported from
+import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //*entire APP has access to the store
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- review/redux/react_redux_initial/src/store/index.js
+++ review/redux/react_redux_initial/src/store/index.js
@@ -1,26 +1,23 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
 //*action is an object
-const counterReducers= (state={counter:0}, action){
-    if(action.type = 'increment'){
-        return{
-            counter: state.counter+1
-        }
-    }
-    if(action.type = 'decrement'){
-        return{
-            counter: state.counter-1
-        }
-    }
+const counterReducers = (state = { counter: 0 }, action) => {
+  if ((action.type = "increment")) {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if ((action.type = "decrement")) {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    return state
-}
+  return state;
+};
 
+const store = createStore(counterReducers);
 
-
-
-const store = createStore(counterReducers)
-
-export default store
+export default store;
 // store.dispatch({type:'increment'})
 // store.dispatch({type:'decrement'})
