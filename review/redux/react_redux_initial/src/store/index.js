d31e173c4748e84c58f907346c413ad52a67f0b9
AltaraceO <orialtarace@gmail.com>
2022-06-07 15:25:04 +0300

    refactor the redux index using toolkit

--- review/redux/react_redux_initial/src/store/index.js
+++ review/redux/react_redux_initial/src/store/index.js
@@ -1,6 +1,28 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+//*Behind the scenes TOOLKIT duplicates the state so it looks like its mutating the original state but it's NOT
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    //*dont have to use action , but it's there for payload
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 //*action is an object
 const counterReducers = (state = initialState, action) => {
